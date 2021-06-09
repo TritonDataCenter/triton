9b09701dc1207c60bd0bf93f67d3ba2c2aa4b8c0
Brian Bennett <brian.bennett@joyent.com>
2021-06-09 11:58:22 -0700

    TRITON-2218 Headnode should support unattended install based on equinix metal metadata (docs) (#289)

--- docs/developer-guide/ipxe-installer.md
+++ docs/developer-guide/ipxe-installer.md
@@ -13,11 +13,10 @@
 Some deployments, for example on to a Bare-Metal as a Service (BMaaS)
 provider, may use single-shot iPXE boots to install an operating system.
 
-The Triton iPXE installer behaves much like the [ISO
-installer](./docs/developer-guide/iso-installer.md) in that it should boot
-once, and during the reboot mid-installation the on-disk portion should take
-over.  It is important that the iPXE Triton boot happen only once for a head
-node installation.
+The Triton iPXE installer behaves much like the [ISO installer][triton-iso-doc]
+in that it should boot once, and during the reboot mid-installation the on-disk
+portion should take over.  It is important that the iPXE Triton boot happen
+only once for a head node installation.
 
 Unlike the ISO installer, the iPXE installation requires that network
 reachability be enabled DURING the installation process.  The installer
@@ -25,34 +24,119 @@ script will post a reminder of this.  Most of the time, the iPXE installer
 needed network reachability to start in the first place, so configuring the
 head node to reach the iPXE downloads should not be difficult.  The network
 is required to be able to download images that are normally on the ISO DVD
-image for the ISO installer, but cannot fit into an iPXE ramdisk root.
+image for the ISO installer, but cannot fit into an iPXE ramdisk root.  This
+network must be at least `/30`. If your provider allocates a `/31`, you should
+configure Triton with a shorter prefix length so that neither the headnode IP
+nor the gateway IP are the network or broadcast numbers. In general this should
+not cause a problem as long as the host and gatway IPs used are those specified
+by the provider's allocation. The minimum *recommended* external network size
+is `/28`.
 
 ## Configuring an iPXE Server for a Triton Installation
 
 An iPXE installation needs to pull necessary files via HTTP/HTTPS for
-installation.  We provide a [tar
-archive](https://us-east.manta.joyent.com/Joyent_Dev/public/SmartDataCenter/ipxe-latest.tgz)
-that can be installed on a web server that an iPXE client can reach.
+installation.  We provide a [tar archive][triton-ipxe] that can be installed on
+a web server that an iPXE client can reach.
 
-The `triton-installer.ipxe` file will need modifications in its `testdomain`
-and `base-url` variables to match your deployment.  If your iPXE webserver is
-ipxe.example.com, and its directory URL is https://ipxe.example.com/triton,
-you would have these lines in triton-installer.ipxe:
+The tar archive also includes a full tar archive of the
+[ISO installer][triton-iso-doc] contents because an iPXE ramdisk image is not
+large enough to hold all of the Triton images required for an installation, and
+the iPXE installer must download the images after it has booted.
 
-```
-	set testdomain ipxe.example.com
-	set base-url https://ipxe.example.com/triton
-```
-
-**NOTE** - The `testdomain` must allow ICMP echo requests and respond to them
-(aka. pings) so the installer can confirm reachability.
-
-The tar archive also includes a full tar archive of the [ISO
-installer](./docs/developer-guide/iso-installer.md) contents because an iPXE
-ramdisk image is not large enough to hold all of the Triton images required
-for an installation, and the iPXE installer must download the images after it
-has booted.
+The included ipxe script defaults to ttyb (COM2) for the installer.  This can
+be changed in the ipxe file if necessary.
 
 Using the example server above, once it has been setup, an iPXE client should
 access `https://ipxe.example.com/triton/triton-installer.ipxe` to install the
 Triton head node.
+
+## Using Joyent's Netboot server
+
+Joyent also provides a netboot server that you can use with iPXE to chain load
+the Triton installer and is kept up to date with the latest release.  To install
+Triton using the iPXE installer, chain load the installer URL.
+
+    chain https://netboot.smartos.org/triton-installer/triton-installer.ipxe
+
+After provisioning your headnode, connect to the serial console to configure
+the installation.  Currently the installer is hard coded to use ttyb (COM2) for
+the installer.
+
+### Automating Triton setup on third-party bare metal hosting providers
+
+Triton has support for fully-automated installation with third-party bare metal
+hosting providers.  This works by loading the hosting provider's metadata object
+onto the filesystem as a boot module.  Currently, only [Equinix Metal][eqm] is
+supported.
+
+#### Equinix Metal
+
+To install Triton on Equinix Metal use the [`triton-eqm-create.sh`][eqm-script]
+script in this repository.  The Triton installer will use the Equinix Metal
+metadata object to derive most values (e.g., IP addresses and interface
+configuration) needed by the installer.  Not all values needed by Triton are
+present in Equinix's metadata object so additional elements can be included by
+providing JSON file.  The file format and keys are the same as
+[`answers.json`][hn] file used by Triton.
+
+The following keys are supported.  For any keys that are not present, a suitable
+default will be used.
+
+| Key                       | Default      |
+| ------------------------- | ------------ |
+| `company_name`            | Empty string |
+| `datacenter_location`     | Empty string |
+| `region_name`             | Leading alpha characters of `datacenter_name`, which will be the Equnix Metal `facility`  (e.g., `iad`) |
+| `dns_resolver1`           | `8.8.8.8` |
+| `dns_resolver2`           | `8.8.4.4` |
+| `dns_domain`              | `triton.local` |
+| `mail_to`                 | `root@localhost` |
+| `mail_from`               | `support@`<dns_domain> |
+| `ntp_host`                | `0.smartos.pool.ntp.org` |
+| `root_password`           | Randomly generated (you will need to use your Equinix Metal SSH keys to log in) |
+| `admin_password`          | Randomly generated.  You can find this password in `/usbkey/config` |
+| `update_channel`          | `release` |
+
+SSH keys present in your Equinix Metal account will be added to `root`'s
+`~/.ssh/authorized_keys` file.
+
+To use the `triton-eqm-create.sh` script you need the [`packet-cli`][p-cli]
+installed and configured on your workstation.  Once this is done, use the
+following steps to create a new Triton cloud.
+
+1. Create a project to contain the assets for your Triton Cloud.  This will also
+   create the necessary VLANs in the specified facility.
+
+        triton-eqm-create.sh project -n My-Triton-Project
+
+2. After the project has been created, you can create the headnode.  Passing an
+   answers file is optional, other values are required.  The hardware plan
+   defaults to `c3.small.x86` but can be changed with `-P`.
+
+        triton-eqm-create.sh headnode -p <project_id> -f sv15 -a my-answers.json
+
+3. Connect to the Equinix Metal "`sos`" console to watch the progress (the
+   correct `ssh` command will be printed for you).  There are some issues that
+   will occasionally prevent properly chain loading the iPXE URL.  If this
+   happens, delete the server and try again.
+
+4. After the installer finishes (which takes about 10 minutes) you will be able
+   to `ssh` as `root` to the headnode's external IP.
+
+5. Refer to the [Triton Operator Documentation][ops-docs] for post-install
+   operation and configuration of your new Triton Cloud.
+
+6. After post-install tasks are complete, create additional Compute Nodes.
+
+        triton-eqm-create.sh computenode -p <project_id> -f iad1
+
+<!-- Footnote style links -->
+
+[triton-ipxe]: https://us-east.manta.joyent.com/Joyent_Dev/public/SmartDataCenter/ipxe-latest.tgz
+[triton-iso-doc]: ./iso-installer.md
+[eqm]: https://metal.equinix.com/
+[tink]: https://tinkerbell.org
+[eqm-script]: ../../tools/triton-eqm-create.sh
+[hn]: https://github.com/joyent/sdc-headnode/
+[p-cli]: https://github.com/packethost/packet-cli
+[ops-docs]: https://docs.joyent.com/private-cloud/
---
+++ tools/triton-eqm-create.sh
@@ -0,0 +1,495 @@
+#!/bin/bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#
+# Copyright 2021 Joyent, Inc.
+#
+
+# shellcheck disable=SC2154
+if [[ -n "$TRACE" ]]; then
+    export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    set -o xtrace
+fi
+
+function stack_trace
+{
+    set +o xtrace
+
+    (( cnt = ${#FUNCNAME[@]} ))
+    (( i = 0 ))
+    while (( i < cnt )); do
+        printf '  [%3d] %s\n' "${i}" "${FUNCNAME[i]}"
+        if (( i > 0 )); then
+            line="${BASH_LINENO[$((i - 1))]}"
+        else
+            line="${LINENO}"
+        fi
+        printf '        (file "%s" line %d)\n' "${BASH_SOURCE[i]}" "${line}"
+        (( i++ ))
+    done
+}
+
+function fatal
+{
+    # Disable error traps from here on:
+    set +o xtrace
+    set +o errexit
+    set +o errtrace
+    trap '' ERR
+
+    echo "$(basename "$0"): fatal error: $*" >&2
+    stack_trace
+    exit 1
+}
+
+function trap_err
+{
+    st=$?
+    fatal "exit status ${st} at line ${BASH_LINENO[0]}"
+}
+
+set -o errexit
+set -o pipefail
+
+shopt -s extglob
+
+#
+# Install our error handling trap, so that we can have stack traces on
+# failures.  We set "errtrace" so that the ERR trap handler is inherited
+# by each function call.
+#
+
+trap trap_err ERR
+set -o errtrace
+
+function usage
+{
+    printf 'Create a new Triton Headnode install in Equinix Metal.\n\n'
+    printf 'Syntax:\n\n'
+    printf '\t%s project -n project_name\n' "$0"
+    printf '\t%s headnode -p project_uuid -f facility -P hardware_plan -a answer_file\n\n' "$0"
+    printf '\t%s computenode -n <cn name> -p project_uuid -f facility\n\n' "$0"
+    exit "$1"
+}
+
+#############
+
+#
+# Global Variables
+#
+
+LOG=$(mktemp "${TMPDIR:-/tmp}/triton-eqm.XXXXXX")
+prefix_l='28'
+
+if [[ -z $PACKET_TOKEN ]]; then
+    if [[ -f ~/.packet-cli.json ]]; then
+        PACKET_TOKEN=$(json -f ~/.packet-cli.json token)
+        export PACKET_TOKEN
+    else
+        fatal 'PACKET_TOKEN is unset and ~/.packet-cli.json does not exist.'
+    fi
+fi
+
+# For subnet calculation
+prefix_num_ips=(
+4294967296
+2147483648
+1073741824
+536870912
+268435456
+134217728
+67108864
+33554432
+16777216
+8388608
+4194304
+2097152
+1048576
+524288
+262144
+131072
+65536
+32768
+16384
+8192
+4096
+2048
+1024
+512
+256
+128
+64
+32
+16
+8
+4
+2
+1
+)
+
+#
+# Functions
+#
+
+function assign_networks
+{
+    project_uuid="$1"
+    server="$2"
+
+    printf 'Assinging networks...'
+
+    current_network_ports=$(packet server get -j -i "$server" | json network_ports)
+    eth1_id=$(json -a -c 'this.name.match(/eth1/)' id <<< "$current_network_ports")
+
+    virtual_networks=$(packet virtual-network get -p "$project_uuid" -j | json virtual_networks)
+    admin_net=$(json -ac 'this.description.match(/Admin/)' id <<< "$virtual_networks")
+    vnids=()
+    while IFS='' read -r line; do vnids+=("$line"); done < <(json -a id <<< "$virtual_networks")
+
+    {
+        # Remove eth1 from the bond
+        call_api "/ports/$eth1_id/disbond" -X POST -d '{"bulk_disable": false}'
+
+        # Add VLANs and set native VLAN.
+        for vnid in "${vnids[@]}"; do
+            call_api "/ports/$eth1_id/assign" -X POST -d '{"vnid":"'"$vnid"'"}'
+        done
+        call_api "/ports/$eth1_id/native-vlan" -X POST -d '{"vnid":"'"$admin_net"'"}'
+    } >> "$LOG"
+
+    printf 'done.\n'
+}
+
+function call_api
+{
+    endpoint='https://api.equinix.com/metal/v1'
+    headers=(
+        -H "X-Auth-Token: $PACKET_TOKEN"
+        -H  "accept: application/json"
+        -H  "Content-Type: application/json"
+    )
+    query="$1" ; shift
+    curl -i -s "${headers[@]}" "${endpoint}${query}" "$@" | json
+}
+
+function create_project
+{
+    local out;
+    local proj_u;
+    project_name="$1"
+
+    out=$(packet project create --name "$project_name" -j)
+    echo "$out" >> "$LOG"
+
+    proj_u="$( json id <<< "$out")"
+    echo "$proj_u"
+}
+
+function create_server
+{
+    local reservation create_json out
+
+    local project_uuid="$1"
+    local facility="$2"
+    local s_hostname="$3"
+
+    # Defaults for CNs
+    local always_ipxe=true
+    local ip_addresses=''
+    local ipxe_url='https://netboot.smartos.org/triton-cn/triton-cn.ipxe'
+
+    if [[ $s_hostname =~ "headnode" ]]; then
+        always_ipxe=false
+        ipxe_url='https://netboot.smartos.org/triton-installer/packet.ipxe'
+    fi
+
+    # Create an IP reservation
+    create_subnet_request "$project_uuid" "$facility" "$s_hostname"
+    # Take the first reservation with no assignments.
+    reservation=$(call_api "/projects/$project_uuid/ips" | json -H ip_addresses | json -ac 'this.address_family==4 && this.public==true && this.assignments.length==0' id | head -1)
+
+    ip_addresses=$(printf ',
+      "public_ipv4_subnet_size": %s,
+      "private_ipv4_subnet_size": 31,
+      "ip_addresses": [
+        {
+          "address_family": 4,
+          "public": true,
+          "cidr": %s,
+          "ip_reservations": [
+            "%s"
+          ]
+        },
+        {
+          "address_family": 4,
+          "public": false
+        }]' "$prefix_l" "$prefix_l" "$reservation")
+
+    create_json=$( printf '
+        {
+          "facility": "%s",
+          "plan": "%s",
+          "hostname": "%s",
+          "operating_system": "custom_ipxe",
+          "always_pxe": %s,
+          "ipxe_script_url": "%s",
+          "customdata": %s
+          %s
+        }' "$facility" "${hardware_plan:-c3.small.x86}" "$s_hostname" \
+            "$always_ipxe" "$ipxe_url" "$customdata" "$ip_addresses"
+    )
+
+    out=$(call_api "/projects/$project_uuid/devices" -X POST -d "$create_json")
+    echo "$out" >> "$LOG"
+    json -H id <<< "$out"
+}
+
+function create_subnet_request
+{
+    project_uuid="$1"
+    facility="$2"
+    hostname="$3"
+    packet ip request -f "$facility" -p "$project_uuid" -t public_ipv4 \
+        -c "IPs for $hostname" -q 16 >> "$LOG"
+}
+
+function create_vlans
+{
+    project_uuid="$1"
+    facility="$2"
+    local networks=(
+        Admin
+        Underlay
+    )
+    existing_vlans=$(packet virtual-network get -p "$project_uuid" -j | \
+        json virtual_networks | json -a description)
+    for net in "${networks[@]}" ; do
+        if ! grep "$net" <<< "$existing_vlans" ; then
+            packet virtual-network create -j -f "$facility" -d "Triton $net" \
+                -p "$project_uuid"
+        fi
+    done >> "$LOG"
+}
+
+function do_setup_project
+{
+    while getopts "n:f:h" options; do
+        case $options in
+            n) project_name="$OPTARG" ;;
+            h) usage 0 ;;
+            *) usage 1 ;;
+        esac
+    done
+
+    if [[ -z $project_name ]]; then
+        usage 1
+    fi
+
+    project_uuid=$(create_project "$project_name")
+
+    ## Summary
+    printf 'Project Name: %s\n' "$project_name"
+    printf 'Project UUID: %s\n' "$project_uuid"
+}
+
+function do_setup_server
+{
+    local facility project_uuid s_hostname server
+
+    printf 'Verbose log is %s\n' "$LOG"
+
+    customdata='{}'
+
+    while getopts "a:f:p:P:n:h" options; do
+        case $options in
+            a) answers_file="$OPTARG";;
+            f) facility="$OPTARG" ;;
+            p) project_uuid="$OPTARG" ;;
+            P) hardware_plan="$OPTARG" ;;
+            n) s_hostname="$OPTARG" ;;
+            h) usage 0 ;;
+            *) usage 1 ;;
+        esac
+    done
+
+    if [[ -z $project_uuid ]] || [[ -z $facility ]] ; then
+        usage 1
+    fi
+
+    if [[ $s_hostname == "headnode" ]]; then
+        # Make sure Packet hostnames are unique within a single project.
+        # This comes into play when there's a multi-facility UFDS replicated
+        # cloud.
+        s_hostname="headnode-$facility"
+    fi
+
+    if [[ $s_hostname =~ "headnode" ]] && [[ -n $answers_file ]]; then
+        customdata="$(cat "$answers_file")"
+    fi
+
+    # This is an idempotent operation.
+    create_vlans "$project_uuid" "$facility"
+
+    printf 'Creating server...'
+    server=$(create_server "$project_uuid" "$facility" "$s_hostname")
+    if [[ -z $server ]]; then
+        # Trigger errexit if we don't have a server UUID
+        false
+    fi
+    printf 'done.\n'
+
+    printf 'Watch server progress:\n\n\t'
+    printf 'ssh %s@sos.%s.platformequinix.com\n\n' "$server" "$facility"
+
+    # wait for server
+    printf 'Waiting for server to complete provisioning...'
+    wait_for_state active
+    printf 'done.\n'
+    assign_networks "$project_uuid" "$server"
+    printf 'Server provisioning complete.\n'
+    if ! [[ $s_hostname =~ "headnode" ]]; then
+        show_napi_commands "$s_hostname" "$server"
+    fi
+}
+
+function get_provisionable_ip
+{
+    local subnet octet
+    local which_end="$2"
+    local network
+    local prefix_l
+
+    IFS=/ read -ra subnet <<< "$1"
+    IFS=. read -ra octet <<< "${subnet[0]}"
+
+    prefix_l="${subnet[1]}"
+    if (( prefix_l > 29)) || (( prefix_l < 24 )); then
+        printf 'Prefix length %s out of range.\n' "${subnet[1]}"
+        return 1
+    fi
+
+    case "$which_end" in
+        start)
+            octet[3]=$(( octet[3] + 2 )) ;;
+        end)
+            octet[3]=$(( octet[3] + ${prefix_num_ips[$prefix_l]} - 2)) ;;
+        *)
+            printf 'can only get start or end ip\n'
+            return 1
+            ;;
+    esac
+
+    printf '%s.%s.%s.%s\n' "${octet[0]}" "${octet[1]}" "${octet[2]}" \
+        "${octet[3]}"
+}
+
+function show_napi_commands
+{
+    local s_hostname="$1"
+    local server="$2"
+    local ip_addresses network_name nic_tag obj
+    local provision_end provision_start
+    local network prefix_l
+
+    nic_tag="external_rack_${s_hostname}r"
+    network_name="external_rack_${s_hostname}r"
+
+    json_tmpl='{
+  "nic_tag": "%s",
+  "name": "%s",
+  "subnet": "%s/%s",
+  "gateway": "%s",
+  "provision_start_ip": "%s",
+  "provision_end_ip": "%s",
+  "vlan_id": 0,
+  "resolvers": [
+    "8.8.8.8",
+    "8.8.4.4"
+  ],
+  "description": "External for %s"}'
+
+    ip_addresses=$(packet server get -i "$server" -j | json ip_addresses | \
+        json -o jsony-0 -ac 'this.public==true')
+
+    printf 'sdc-napi /nic_tags -X POST -d '
+    printf \'
+    printf '{"name":"%s"}' "$nic_tag"
+    printf \''\n'
+
+    while read -r obj; do
+        gateway=$(json gateway <<< "$obj")
+        network=$(json network <<< "$obj")
+        prefix_l=$(json cidr <<< "$obj")
+
+        provision_start=$(get_provisionable_ip "${network}/${prefix_l}" start)
+        provision_end=$(get_provisionable_ip "${network}/${prefix_l}" end)
+
+        # shellcheck disable=SC2059
+        json=$(
+            printf "$json_tmpl" "$nic_tag" "$network_name" \
+                "$network" "$prefix_l" \
+                "$gateway" \
+                "$provision_start" \
+                "$provision_end" \
+                "$s_hostname" | json -o json-0
+        )
+
+        printf 'sdc-napi /networks -X POST -d '
+        printf \'
+        printf '%s' "$json"
+        printf \''\n'
+    done <<< "$ip_addresses"
+}
+
+function wait_for_state
+{
+    desired_state="$1"
+    current_state='unknown'
+
+    while [[ $current_state != "$desired_state" ]]; do
+        # Show some progress while waiting.
+        sleep 5 ; printf '.'
+        current_state=$(packet server get -j -i "$server" | json state)
+    done
+
+}
+
+##
+## Main
+##
+
+while getopts "h" options; do
+    case $options in
+        h) usage 0 ;;
+        *) echo "$OPTARG" ;;
+    esac
+done
+
+action="$1"
+
+if [[ -z $action ]]; then
+    usage 1
+fi
+
+shift
+
+case "$action" in
+    project)
+        do_setup_project "$@" ;;
+    headnode)
+        do_setup_server -n headnode "$@" ;;
+    cn|compute|computenode)
+        n="cn$(openssl rand -hex 3)"
+        do_setup_server -n "$n" "$@" ;;
+    sn)
+        hostname="$1"
+        uuid="$2"
+        show_napi_commands "${hostname:?}" "${uuid:?}"
+        ;;
+    *)
+        usage 1 ;;
+esac
